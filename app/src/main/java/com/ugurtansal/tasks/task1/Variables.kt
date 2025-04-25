c3134491e68853e6693789f51a603f18ae7d2b19
Uğur Tansal <ugur.tansal@gmail.com>
2025-04-25 16:20:51 +0300

    First task

---
+++ app/src/main/java/com/ugurtansal/tasks/task1/Variables.kt
@@ -0,0 +1,54 @@
+package com.ugurtansal.tasks.task1
+
+fun main() {
+    // var ile tanımlananlar (değiştirilebilir)
+    var city = "Istanbul"
+    var country = "Turkey"
+    var phone = "+905551234567"
+    var postalCode = "34000"
+    var email = "example@mail.com"
+    var profession = "Engineer"
+    var stockAmount = 150
+    var customerName = "John Doe"
+    var balance = 1250.75
+    var birthday = "1995-06-15"
+    var salary = 8000.0
+    var maritalStatus = "Single"
+    var productReview = "Excellent product!"
+    var paymentDate = "2025-04-20"
+    var payment = 299.99
+    var orderQuantity = 3
+    var carModel = "Tesla Model 3"
+    var bookTitle = "The Great Gatsby"
+    var publishingDate = "1925-04-10"
+    var discountAmount = 50.0
+    var roomNumber = 12
+    var latitude = 41.0082
+    var longitude = 28.9784
+    var productName = "Smartphone"
+    var foodPrice = 120.5
+    var brand = "Apple"
+    var musicTitle = "Bohemian Rhapsody"
+    var videoDuration = "3:45"
+    var productRating = 4.8
+    var imageName = "nature_photo"
+    var fileFormat = "JPEG"
+    var color = "Red"
+    var colorCode = "#FF0000"
+
+// val ile tanımlananlar (değiştirilemez)
+    val phoneModel = "iPhone 15"
+    val screenSize = 6.1
+    val weight = 174.0
+    val nationalDay = "October 29"
+    val holiday = "New Year's Day"
+    val reservationDate = "2025-05-10"
+    val streetName = "Main Street"
+    val busLine = "34AS"
+    val remainingMinutes = 12
+    val trackingCode = "TRK123456789"
+    val couponDuration = 30 // days
+    val couponCode = "SAVE20"
+    val billingAddress = "123 Elm Street, NY"
+
+}
