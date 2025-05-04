3e01267c2c21def80956b3eb3b007a7a9ca81ead
Uğur Tansal <ugur.tansal@gmail.com>
2025-05-05 00:44:43 +0300

    Finished passing between fragments

--- task-4/app/src/main/java/com/ugurtansal/task_4/XFragment.kt
+++ task-4/app/src/main/java/com/ugurtansal/task_4/XFragment.kt
@@ -5,55 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
+import com.ugurtansal.task_4.databinding.FragmentMainBinding
+import com.ugurtansal.task_4.databinding.FragmentXBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [XFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class XFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentXBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_x, container, false)
+
+        binding= FragmentXBinding.inflate(inflater, container, false)
+
+        binding.buttonXtoY.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.X_to_Y_pass)
+        }
+
+        val backPress= object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle the back press event
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backPress)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment XFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            XFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
