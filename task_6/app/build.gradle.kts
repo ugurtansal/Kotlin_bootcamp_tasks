3a2b1bd8cc212b7f3f7a6a49affd958698a105bc
Uğur Tansal <ugur.tansal@gmail.com>
2025-05-14 16:41:46 +0300

    Designed bottom app bar

--- task_6/app/build.gradle.kts
+++ task_6/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
 }
 
 android {
@@ -51,4 +52,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
 }
--- task_6/app/src/main/java/com/ugurtansal/task_6/MainActivity.kt
+++ task_6/app/src/main/java/com/ugurtansal/task_6/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+//        val navController = findNavController(R.id.fragmentContainerView)
+//        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        bottomNav.setupWithNavController(navController)
 
     }
 }
