b3f1fa0ae8de42c1080bf069cd7a4709a6b63795
Uğur Tansal <ugur.tansal@gmail.com>
2025-05-18 16:05:36 +0300

    Prepared repository and data source classes

--- task_7/app/src/main/java/com/ugurtansal/task_7/data/dataSource/ToDoDataSource.kt
+++ task_7/app/src/main/java/com/ugurtansal/task_7/data/dataSource/ToDoDataSource.kt
@@ -1,6 +1,33 @@
 package com.ugurtansal.task_7.data.dataSource
 
+import com.ugurtansal.task_7.data.entity.ToDo
 import com.ugurtansal.task_7.room.ToDoDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class ToDoDataSource (toDoDao: ToDoDao) {
+class ToDoDataSource (var toDoDao: ToDoDao) {
+
+    suspend fun save(task: String) {
+        val newTask = ToDo(0, task)
+
+        toDoDao.addTask(newTask)
+    }
+
+    suspend fun update(taskId: Int, task: String) {
+        val updatedTask = ToDo(taskId, task)
+        toDoDao.updateTask(updatedTask)
+    }
+
+    suspend fun delete(taskId: Int) {
+        val taskToDelete = ToDo(taskId, "")
+        toDoDao.deleteTask(taskToDelete)
+    }
+
+    suspend fun loadTasks(): List<ToDo> = withContext(Dispatchers.IO) {
+        return@withContext toDoDao.loadTasks()
+    }
+
+    suspend fun search(searchedWord: String): List<ToDo> = withContext(Dispatchers.IO) {
+        return@withContext toDoDao.search(searchedWord)
+    }
 }
--- task_7/app/src/main/java/com/ugurtansal/task_7/data/repo/ToDoRepository.kt
+++ task_7/app/src/main/java/com/ugurtansal/task_7/data/repo/ToDoRepository.kt
@@ -1,7 +1,24 @@
 package com.ugurtansal.task_7.data.repo
 
 import com.ugurtansal.task_7.data.dataSource.ToDoDataSource
+import com.ugurtansal.task_7.data.entity.ToDo
 
 class ToDoRepository (var toDoDataSource: ToDoDataSource) {
+
+    suspend fun save(task: String) {
+        toDoDataSource.save(task)
+    }
+
+    suspend fun update(taskId: Int, task: String) {
+        toDoDataSource.update(taskId, task)
+    }
+
+    suspend fun delete(taskId: Int) {
+        toDoDataSource.delete(taskId)
+    }
+
+    suspend fun loadTasks(): List<ToDo> = toDoDataSource.loadTasks()
+
+    suspend fun search(searchedWord: String) : List<ToDo> = toDoDataSource.search(searchedWord)
 
 }
